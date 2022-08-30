c928d7debc2d35f0e6a8fd8d8ebad938286fd133
annafitzgerald <fitzgeralda1403@gmail.com>
2022-08-30 15:14:32 -0400

    My first java program. Update file to include today's changes.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
 // By convention, class names are capitalized.
 public class HelloWorld {
+
+    /*
+    This is a multiline block comment.
+    Another comment line after the first...
+     */
+
     public static void main(String [] args) {
         System.out.println("Hello World");
         int x = 5;
         boolean z= true;
         double pi = 3.14159;
+        String name = "CS129 Techniques of Computer Science";
+
+        double avagadors = 6.022140857e23; // Scientific notation
+        double electron_mass = 9.1093837015e-31;
     }
 }
