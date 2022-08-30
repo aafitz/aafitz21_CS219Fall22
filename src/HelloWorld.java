53421f0c316038c2a5f93ea8de504f3d078fd08f
annafitzgerald <fitzgeralda1403@gmail.com>
2022-08-30 15:57:17 -0400

    My first java program. Update file to include today's changes.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
         double pi = 3.14159;
         String name = "CS129 Techniques of Computer Science";
 
-        double avagadors = 6.022140857e23; // Scientific notation
+        double avogadros = 6.022140857e23; // Scientific notation
         double electron_mass = 9.1093837015e-31;
     }
 }
