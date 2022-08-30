53a66bd08b9f67119b548b42c35005ac00fd204f
annafitzgerald <fitzgeralda1403@gmail.com>
2022-08-30 15:52:18 -0400

    My first java program. Update file to include today's changes.

--- src/Investment.java
+++ src/Investment.java
@@ -11,7 +11,7 @@ public class Investment {
        // double p = c * Math.pow(1 + r/n, t*n);
 
         System.out.println(investment( 1.0,  1.0,  1,(int)1e9)); // must call the function "investment" here
-
-
+    // can change the above numbers tp whatever they may be... as long as they matched what type you declared each variable to be
+        System.out.println(investment( 1000, 0.03, 10, 365));
     }
 }
