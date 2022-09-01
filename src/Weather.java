1c0137b71777adae61b330e7e8f814a556ada59e
annafitzgerald <fitzgeralda1403@gmail.com>
2022-09-01 15:09:47 -0400

    In class assignment.

--- src/Weather.java
+++ src/Weather.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Weather {
 public static double windchill(double t, // Temperature
-                               double v) { // velocity
+                               double v) { // velocity in MPH
     return 35.75 + 0.6215*t + (0.4275*t -35.75)*Math.pow(v, 0.16);
 
 }
 public static void main(String [] args){
-    System.out.println(windchill(32, 10));
+
+    // used for getting input from the keyboard
+    Scanner kbd = new Scanner(System.in);
+
+    System.out.print("Enter temperature in degrees F: ");
+
+    double temp = kbd.nextDouble();
+
+    System.out.print("Enter a wind velocity in MPH: ");
+
+    double velocity = kbd.nextDouble();
+
+
+
+    System.out.printf("Windchill is %.2f\n", + windchill(temp,velocity));
 }
 }
