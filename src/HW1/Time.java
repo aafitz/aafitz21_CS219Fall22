8f1db85230c931a036edb22684ce311f75bfcfc9
annafitzgerald <fitzgeralda1403@gmail.com>
2022-09-01 10:40:26 -0400

    HW Exercise 1.

---
+++ src/HW1/Time.java
@@ -0,0 +1,18 @@
+package HW1;
+
+public class Time {
+    public static void main(String[] args) {
+        int hour = 15;
+        int minute = 0;
+        int second = 0;
+        int passed = Math.round((hour * 60 * 60) + (minute * 60) + second);
+        int day = 24 * 60 * 60;
+        double percent = (((double)passed / day) * 100);
+
+        System.out.println(hour + ":" + minute + ":" + second +" is " + passed + " seconds since midnight");
+        System.out.println("There are " + (day - passed) + " seconds remaining in the day.");
+        System.out.println(percent + "%" + " of the day has elapsed.");
+    }
+
+
+}
