423af1ab10cb798e9cb29568547380aad653e16a
annafitzgerald <fitzgeralda1403@gmail.com>
2022-10-12 17:40:25 -0400

    HW5

--- src/HW5/Exercise66.java
+++ src/HW5/Exercise66.java
@@ -1,39 +1,33 @@
- /* package HW5;
+ package HW5;
 
 public class Exercise66 {
     public static boolean canSpell(String word, String tiles) {
-        String Check = '';
-
-        for (int i =0; i < word.length(); i++) {
+        boolean canSpellNow = false;
+        for (int i = 0; i < word.length(); i++) {
             char word_char = word.charAt(i);
-
 
             for (int i = 0; i < tiles.length(); i++) {
                 char tiles_char = tiles.charAt(i);
 
                 // store the character of the indicie
                 if (word_char == tiles_char) {
-                    tiles_char += Check;
-                    tiles = tiles.charAt(0, i) + tiles.substring(i + 1);
-                    break;
+                    word = word.substring(0, word.indexOf(word_char)) + word.substring(word.indexOf(word_char) + 1);
                 }
             }
         }
-        // if they dont match then keep going forward-- boolean false
-        return Check.equals(word); /// if they do match then boolean = true
+        if (word.length() == 0)
+            canSpellNow = true;
+        return canSpellNow;
+    }
+        /// if they do match then boolean = true
 
 
+        public static void main (String[]args){
+            System.out.println(canSpell("boot", "axobasrto"));
+            System.out.println(!canSpell("hippo", "haxobapsrito"));
+            System.out.println(canSpell("anna", "bgtydaynewna"));
 
-
-    public static void main(String [] args) {
-        System.out.println(canSpell("boot", "axobasrto"));
-        System.out.println(!canSpell("hippo", "haxobapsrito"));
-        System.out.println(canSpell("anna"))
-
-        // Student: add more test cases
+            // Student: add more test cases
+        }
     }
 }
-}
-/*
-/*
-  */
